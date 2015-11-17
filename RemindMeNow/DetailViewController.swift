9d06899c45e786135635cc865bbd5ace6154debf
HardcoreIOS <Abdurrahman@owners-mbp.home>
2015-11-17 11:26:57 -0500

    first commit

---
+++ RemindMeNow/DetailViewController.swift
@@ -0,0 +1,75 @@
+//
+//  DetailViewController.swift
+//  RemindMe
+//
+//  Created by Abdurrahman on 9/22/15.
+//  Copyright © 2015 Hafiz Developer. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var notesTextView: UITextView!
+    
+    @IBAction func trashItem(sender: AnyObject) {
+        
+        var userDefaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        
+        var itemListArray: NSMutableArray = userDefaults.objectForKey("itemList") as! NSMutableArray
+        
+        var mutableItemList: NSMutableArray = NSMutableArray()
+        
+        for dict:AnyObject in itemListArray {
+            
+            mutableItemList.addObject(dict as! NSDictionary)
+            
+            mutableItemList.removeObject(toDoData)
+            
+            userDefaults.removeObjectForKey("itemList")
+            
+            userDefaults.setObject(mutableItemList, forKey: "itemList")
+            
+            userDefaults.synchronize()
+            
+            self.navigationController?.popToRootViewControllerAnimated(true)
+            
+        }
+        
+        
+        
+    }
+    
+    
+    var toDoData: NSDictionary = NSDictionary()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleTextField.userInteractionEnabled = false
+        notesTextView.userInteractionEnabled = false
+        
+        titleTextField.text = toDoData.objectForKey("itemTitle") as! String
+        notesTextView.text = toDoData.objectForKey("itemNote") as! String
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
--- RemindMeNow/ViewController.swift
+++ RemindMeNow/ViewController.swift
@@ -1,25 +1,67 @@
 //
 //  ViewController.swift
-//  RemindMeNow
+//  RemindMe
 //
-//  Created by Abdurrahman on 11/17/15.
+//  Created by Abdurrahman on 9/22/15.
 //  Copyright © 2015 Hafiz Developer. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var notesTextView: UITextView!
+    @IBAction func addButtonTapped(sender: AnyObject) {
+        
+        var userDefaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        
+        var itemList: NSMutableArray? = userDefaults.objectForKey("itemList") as? NSMutableArray
+        
+        var dataSet: NSMutableDictionary = NSMutableDictionary()
+        dataSet.setObject(titleTextField.text!, forKey: "itemTitle")
+        dataSet.setObject(notesTextView.text, forKey: "itemNote")
+        
+        if (itemList != nil) {
+            var newMutableList: NSMutableArray = NSMutableArray()
+            
+            for dict:AnyObject in itemList! {
+                newMutableList.addObject(dict as! NSDictionary)
+            }
+            
+            userDefaults.removeObjectForKey("itemList")
+            newMutableList.addObject(dataSet)
+            userDefaults.setObject(newMutableList, forKey: "itemList")
+            
+            
+        } else {
+            userDefaults.removeObjectForKey("itemList")
+            itemList = NSMutableArray()
+            itemList!.addObject(dataSet)
+            userDefaults.setObject(itemList, forKey: "itemList")
+        }
+        
+        userDefaults.synchronize()
+        
+        self.navigationController?.popToRootViewControllerAnimated(true)
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if NSUserDefaults.standardUserDefaults().objectForKey("itemList") != nil {
+            NSUserDefaults.standardUserDefaults().objectForKey("itemList")
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
